5ac3c7f17310fcd435c05606a811e6b748f0e566
samar456 <84350876+samar456@users.noreply.github.com>
2023-05-07 02:04:12 +0530

    Updates

---
+++ precache-manifest.197535c799a038c0ede216d93fc1ed1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f29454e98cb75d23681e",
+    "url": "/TextUtils/static/css/main.4b1e8ffe.chunk.css"
+  },
+  {
+    "revision": "f29454e98cb75d23681e",
+    "url": "/TextUtils/static/js/main.f29454e9.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TextUtils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/TextUtils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "eeac072fcc85ca05faa8",
+    "url": "/TextUtils/static/js/runtime~main.eeac072f.js"
+  },
+  {
+    "revision": "ad2fa0745c96e10626c9a48d7dace0b6",
+    "url": "/TextUtils/index.html"
+  }
+];
